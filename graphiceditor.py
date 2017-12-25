f5fd303249849a048b2c3910e46e4ce30bb6b047
zadinvit <vitek.zadina@gmail.com>
2017-12-25 13:13:03 +0100

    inverze

--- graphiceditor.py
+++ graphiceditor.py
@@ -1,12 +1,15 @@
 import numpy as np
 import sys
 from PIL import Image
+def inverze(image_name):
+    # práce s obrázky inverzní obraz
+    img = Image.open(image_name)
+    imgarr = np.asarray(img)
+    imgarr = 255 - imgarr
+    j = Image.fromarray(imgarr)
+    image_out=input("Uložit obrázek jako: ")
+    j.save(image_out)
+    print("Obrázek byl uložen jako:" + image_out)
+image_name= input("Zadejte cestu k obrázku: \n")
+inverze(image_name)
 
-image_name="kvetina.jpg"
-#práce s obrázky inverzní obraz
-img = Image.open(image_name)
-imgarr = np.asarray(img)
-print(imgarr)
-imgarr = 255-imgarr
-j = Image.fromarray(imgarr)
-j.save("kvetina"+"out.jpeg")
