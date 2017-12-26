5e103c5d96c3d517f47e04a6cd021e33d3570c1a
zadinvit <vitek.zadina@gmail.com>
2017-12-26 18:11:48 +0100

    UI update

--- graphiceditor.py
+++ graphiceditor.py
@@ -1,17 +1,28 @@
 import numpy as np
 import sys
 from PIL import Image
-def inverze(image_name):
+def inverze(img):
     # práce s obrázky inverzní obraz
-    img = Image.open(image_name)
     imgarr = np.asarray(img)
     imgarr = 255 - imgarr
-    j = Image.fromarray(imgarr)
-    image_out=input("Uložit obrázek jako: ")
-    j.save(image_out)
-    print("Obrázek byl uložen jako: " + image_out)
-image_name= input("Zadejte cestu k obrázku: \n")
-uprava= input("1) Inverze \nZadejte číslo úpravy: \n")
-if int(uprava) == 1:
-    inverze(image_name)
+    imgarr = Image.fromarray(imgarr)
+    return imgarr
 
+def levels(img_arr):
+    level = input("Pro ztmavení zadejte desetinné číslo od 0-1.0 pro zesvětlení číslo >1.0\n")
+    img2 = img_arr.point(lambda p: p * float(level))
+    return img2
+
+image_name = input("Zadejte cestu k obrázku: \n")
+img = Image.open(image_name)
+
+uprava= input("1) Inverze\n2) Úrovně (zesvětlit, ztmavit) \nZadejte číslo úpravy: \n")
+if int(uprava) == 1:
+    fin_img = inverze(img)
+elif int(uprava) == 2:
+    fin_img = levels(img)
+
+image_out=input("Uložit obrázek jako: ")
+
+fin_img.save(image_out)
+print("Obrázek byl uložen jako: " + image_out)
