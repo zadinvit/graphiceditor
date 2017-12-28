5831d32a565300aba52d1d93b76954f294987950
zadinvit <vitek.zadina@gmail.com>
2017-12-28 11:47:03 +0100

    Zvýraznění hran, ošetření nesprávné cesty k obrázku

--- graphiceditor.py
+++ graphiceditor.py
@@ -12,7 +12,7 @@ def levels(img_arr):
     level = input("Pro ztmavení zadejte desetinné číslo od 0-1.0 pro zesvětlení číslo >1.0\n")
     imgarr = np.asarray(img_arr)
     img2=np.copy(imgarr)
-    imgarr.setflags(write=1) #nutno aby se dalo přepisovat hodnotu v poli, pole je jinak read only
+    imgarr.setflags(write = 1) #nutno aby se dalo přepisovat hodnotu v poli, pole je jinak read only
 
     for i in range(3):
 
@@ -45,17 +45,51 @@ def seda(img):
             imgarr2[i][j][2] = imgarr[i][j][0] * 0.299 + imgarr[i][j][2] * 0.114 + imgarr[i][j][1] * 0.587
     fin_img = Image.fromarray(imgarr2)
     return  fin_img
+def hrany_filter(data):
+    output = np.zeros(data.shape[:2])
+    output = (
+            -data[0:-2, 0:-2] - data[0:-2, 1:-1] - data[0:-2, 2:] -
+            data[1:-1, 0:-2] + 9 * data[1:-1, 1:-1] - data[1:-1, 2:] -
+            data[2:, 0:-2] - data[2:, 1:-1] - data[2:, 2:]
+    )
+    return output
+def hrany(img):
+    imgarr = np.asarray(img, dtype=np.float)
+    X, Y, Z = imgarr.shape
+    fin = np.zeros([X - 2, Y - 2, Z])
 
-image_name = input("Zadejte cestu k obrázku: \n")
-img = Image.open(image_name)
+    # aplikace filtru
+    for i in range(3):
+        fin[:, :, i] = hrany_filter(imgarr[:, :, i])
+    fin_img = np.clip(fin, 0, 255)
+    fin_img = np.asarray(fin_img, dtype=np.uint8)
+    img_out = Image.fromarray(fin_img)
+    return img_out
 
-uprava= input("1) Inverze\n2) Úrovně (zesvětlit, ztmavit)\n3) Převést obrázek do úrovní šedi \nZadejte číslo úpravy: \n")
+bez=1
+while bez:
+    try:
+        image_name = input("Zadejte cestu k obrázku: \n")
+        img = Image.open(image_name)
+        bez=0
+        pass
+    except IOError as ioe:
+        print('\033[41m'+"Tento obrázek neexistuje!"+ '\033[0m')
+        pass
+
+
+
+uprava= input("1) Inverze\n2) Úrovně (zesvětlit, ztmavit)\n3) Převést obrázek do úrovní šedi \n4) Zvýraznění hran \nZadejte číslo úpravy: \n")
 if int(uprava) == 1:
     fin_img = inverze(img)
 elif int(uprava) == 2:
     fin_img = levels(img)
 elif int(uprava) == 3:
     fin_img = seda(img)
+elif int(uprava) == 4:
+    fin_img = hrany(img)
+else:
+    sys.exit()
 
 image_out=input("Uložit obrázek jako: ")
 
